0c9cca3b0841f902bb9d90b552571e547013c09d
nosanet <nosanet@ya.ru>
2023-07-30 13:46:13 +0300

    refactor: index.js change link on deploy

--- library/index.js
+++ library/index.js
@@ -1,2 +1,2 @@
-console.log('1. Task: https://github.com/rolling-scopes-school/tasks/blob/master/tasks/library/library-part1.md\n2. Deploy: https://nosanet.github.io/rsschool-cv/library/\n3. Done 30.07.2023 / Deadline 31.07.2023\n4. Score: 100/100' );
+console.log('1. Task: https://github.com/rolling-scopes-school/tasks/blob/master/tasks/library/library-part1.md\n2. Deploy: https://rolling-scopes-school.github.io/nosanet-JSFEPRESCHOOL2023Q2/library/\n3. Done 30.07.2023 / Deadline 31.07.2023\n4. Score: 100/100' );
 
