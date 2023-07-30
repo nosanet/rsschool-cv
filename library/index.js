e44423b2c8b0781f70e0a46920e18d495d1dccb5
nosanet <nosanet@ya.ru>
2023-07-30 13:42:27 +0300

    feat: add index.js

---
+++ library/index.js
@@ -0,0 +1,2 @@
+console.log('1. Task: https://github.com/rolling-scopes-school/tasks/blob/master/tasks/library/library-part1.md\n2. Deploy: https://nosanet.github.io/rsschool-cv/library/\n3. Done 30.07.2023 / Deadline 31.07.2023\n4. Score: 100/100' );
+
